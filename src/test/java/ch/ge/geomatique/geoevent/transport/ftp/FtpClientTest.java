9ac1c94b731b91528b7a8e31908e5f0e04a2baf5
DEPOLP <Philippe.DePol@etat.ge.ch>
2020-10-21 16:48:20 +0200

    Ajout classes de test.

---
+++ src/test/java/ch/ge/geomatique/geoevent/transport/ftp/FtpClientTest.java
@@ -0,0 +1,94 @@
+package ch.ge.geomatique.geoevent.transport.ftp;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockftpserver.fake.FakeFtpServer;
+import org.mockftpserver.fake.UserAccount;
+import org.mockftpserver.fake.filesystem.DirectoryEntry;
+import org.mockftpserver.fake.filesystem.FileEntry;
+import org.mockftpserver.fake.filesystem.FileSystem;
+import org.mockftpserver.fake.filesystem.WindowsFakeFileSystem;
+
+/**
+* The FtpClientTest class is a Junit test case which connects to a ftp server and downloads a file
+*
+* @author  Philippe De Pol
+* @version 1.0
+* @since   21.10.2020
+*/
+public class FtpClientTest
+{
+
+    private FakeFtpServer fakeFtpServer;
+    
+    private FtpClient ftpClient;
+    
+    String server;
+    String user;
+    String password;
+    String serverFolder;
+    String fileName;
+    String localFolder;
+    String remoteFolder;
+    
+	@Before
+	// Set up the fake ftp server and create an instance of the ftp client
+	public void setUp() throws Exception
+	{
+        server = "localhost";
+        user = "user";
+        password = "password";
+        serverFolder = "";
+        fileName = "file1.txt";
+        localFolder = "D:\\tmp\\";
+        remoteFolder = "C:\\data";
+        
+		fakeFtpServer = new FakeFtpServer();
+		fakeFtpServer.addUserAccount(new UserAccount(user, password, remoteFolder));
+
+		FileSystem fileSystem = new WindowsFakeFileSystem();
+		fileSystem.add(new DirectoryEntry(remoteFolder));
+		fileSystem.add(new FileEntry(remoteFolder + "\\" + fileName, "abcdef 1234567890"));
+		fakeFtpServer.setFileSystem(fileSystem);
+
+		fakeFtpServer.start();
+		
+        File directory = new File(localFolder);
+		if (!directory.exists())
+			directory.mkdir();
+		
+    	ftpClient = new FtpClient(server, user, password, serverFolder, fileName, localFolder);
+
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		// Delete the downloaded file
+		File file = new File(localFolder + fileName);
+		file.delete();
+	}
+
+	@Test
+	public void testDownloadFile() {
+		try
+		{
+			ftpClient.downloadFile();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		File file = new File(localFolder + fileName);
+		assertTrue(file.exists());
+	}
+
+}
---
+++ src/test/java/ch/ge/geomatique/geoevent/transport/ftp/SFtpClientTest.java
@@ -0,0 +1,82 @@
+package ch.ge.geomatique.geoevent.transport.ftp;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.github.stefanbirkner.fakesftpserver.rule.FakeSftpServerRule;
+
+/**
+* The SFtpClientTest class is a Junit test case which connects to a sftp server and downloads a file
+*
+* @author  Philippe De Pol
+* @version 1.0
+* @since   21.10.2020
+*/
+public class SFtpClientTest
+{
+	@Rule
+	public final FakeSftpServerRule sftpServer = new FakeSftpServerRule().addUser("user", "password").setPort(22);
+	
+    private SFtpClient SFtpClient;
+    
+    String server;
+    String user;
+    String password;
+    String serverFolder;
+    String fileName;
+    String localFolder;
+    String privateKey;
+    
+	@Before
+	// Set up the fake sftp server and create an instance of the ftp client
+	public void setUp() throws Exception
+	{
+        server = "localhost";
+        user = "user";
+        password = "password";
+        serverFolder = "";
+        fileName = "file.txt";
+        localFolder = "D:\\tmp\\";
+        privateKey = "";
+		
+        sftpServer.putFile("/" + fileName, "content of file", UTF_8);
+        
+        File directory = new File(localFolder);
+		if (!directory.exists())
+			directory.mkdir();
+		 
+        SFtpClient = new SFtpClient(server, user, password, serverFolder, fileName, localFolder, privateKey);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		// Delete the downloaded file
+		File file = new File(localFolder + fileName);
+		file.delete();
+	}
+
+	@Test
+	public void testDownloadFile()
+	{
+		try
+		{
+			SFtpClient.downloadFile();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exception during testDownloadFile:" + e);
+		}
+
+		File file = new File(localFolder + fileName);
+		assertTrue(file.exists());
+	}
+
+}
