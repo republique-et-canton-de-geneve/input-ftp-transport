ee4de11ae3c07fd73b10035267ce235a4fde47a5
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2020-12-18 17:36:28 +0100

    Fichiers GitHub de mise en open source

--- src/main/java/ch/ge/geomatique/geoevent/transport/ftp/FTPInboundTransport.java
+++ src/main/java/ch/ge/geomatique/geoevent/transport/ftp/FTPInboundTransport.java
@@ -1,3 +1,21 @@
+/*
+ * input-ftp-transport
+ *
+ * Copyright (C) 2019 - 2020 République et Canton de Genève
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ch.ge.geomatique.geoevent.transport.ftp;
 
 import java.nio.ByteBuffer;
@@ -22,7 +40,6 @@ import com.esri.ges.framework.i18n.BundleLogger;
 import com.esri.ges.framework.i18n.BundleLoggerFactory;
 import com.esri.ges.transport.InboundTransportBase;
 import com.esri.ges.transport.TransportDefinition;
-
 
 /**
  * The FTPInboundTransport class implements an ftp transport component which
--- src/main/java/ch/ge/geomatique/geoevent/transport/ftp/SFtpClient.java
+++ src/main/java/ch/ge/geomatique/geoevent/transport/ftp/SFtpClient.java
@@ -1,3 +1,21 @@
+/*
+ * input-ftp-transport
+ *
+ * Copyright (C) 2019 - 2020 République et Canton de Genève
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ch.ge.geomatique.geoevent.transport.ftp;
 
 import java.io.IOException;
@@ -8,7 +26,6 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
-
 
 /**
 * The SFtpClient class allows to connect to a sftp server and downloads a file
